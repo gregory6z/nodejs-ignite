a5aa6e979556eaedffbbcf20192d691934e370cd
gregory6z <gregoryrag@gmail.com>
2022-03-02 11:00:23 +0100

    ts-node-dev

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello World" });
+});
+
 app.listen(3333, () => console.log("server is running"));
