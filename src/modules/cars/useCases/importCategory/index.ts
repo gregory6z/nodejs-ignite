371c042abf930118c9234315c5fb27b63da0e715
gregory6z <gregoryrag@gmail.com>
2022-03-05 21:45:16 +0100

    criando o use case para importar categorias

---
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -0,0 +1,8 @@
+import { ImportCategoryController } from "./ImportCategoryController";
+import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
+
+const importCategoryUseCase = new ImportCategoryUseCase();
+
+export const importCategoryController = new ImportCategoryController(
+  importCategoryUseCase
+);
