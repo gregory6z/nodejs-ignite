bcbe819fb7598bfb917cea0b3aec50aa113d665c
gregory6z <gregoryrag@gmail.com>
2022-03-07 13:25:40 +0100

    removendo os arquivos de upload

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -30,6 +30,7 @@ export class ImportCategoryUseCase {
           });
         })
         .on("end", () => {
+          fs.promises.unlink(file.path);
           resolve(categories);
         })
         .on("error", (err) => {
