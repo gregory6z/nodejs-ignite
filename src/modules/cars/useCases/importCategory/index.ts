f4f5da9735c270888b17171a4f326af2211dcfbb
gregory6z <gregoryrag@gmail.com>
2022-03-07 12:07:19 +0100

    lendo os dados do upload

--- src/modules/cars/useCases/importCategory/index.ts
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -1,7 +1,9 @@
+import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepositories";
 import { ImportCategoryController } from "./ImportCategoryController";
 import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
 
-const importCategoryUseCase = new ImportCategoryUseCase();
+const categoriesRepository = CategoriesRepository.getInstance();
+const importCategoryUseCase = new ImportCategoryUseCase(categoriesRepository);
 
 export const importCategoryController = new ImportCategoryController(
   importCategoryUseCase
