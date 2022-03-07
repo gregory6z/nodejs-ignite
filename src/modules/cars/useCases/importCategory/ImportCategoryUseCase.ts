eb2787bde452645797c88f75233863b5cc64edea
gregory6z <gregoryrag@gmail.com>
2022-03-07 11:42:15 +0100

    conceito de stram

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,5 +1,16 @@
+import { parse } from "csv-parse";
+import fs from "fs";
+
 export class ImportCategoryUseCase {
-  execute(file: any) {
-    console.log(file);
+  execute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = parse();
+
+    stream.pipe(parseFile);
+
+    parseFile.on("data", async (line) => {
+      console.log(line);
+    });
   }
 }
